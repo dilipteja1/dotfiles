2b25d0b4e4bb7509d6fdd8fc4b3c3d2dfb66632f
dilipteja1 <tejadilip23898@gmail.com>
2025-07-22 01:52:24 -0400

    nitrogen picom polybar rofi starship

---
+++ .config/i3/scripts/monitor_setup.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --primary --mode 3072x1920 --pos 0x248 --rotate normal --output HDMI-1 --off --output DP-1 --off --output DP-4 --off --output DP-1-0.2 --off --output DP-1-0 --mode 3840x2160 --pos 3072x0 --rotate normal --output DP-1-1 --off --output DP-1-2 --off --output DP-1-3 --off --output HDMI-1-0 --off
